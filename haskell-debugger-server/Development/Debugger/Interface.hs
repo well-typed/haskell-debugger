e4dca5a951e2a4d74c402f7d3521d9bfec992e82
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-03-26 09:07:43 +0000

    Comm: Handle abort

--- haskell-debugger-server/Development/Debugger/Interface.hs
+++ haskell-debugger-server/Development/Debugger/Interface.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE LambdaCase, RecordWildCards #-}
 module Development.Debugger.Interface where
 
+import qualified Data.Text as T
 import Control.Concurrent.MVar
 import Control.Monad.IO.Class
 
@@ -13,11 +14,8 @@ import Development.Debugger.Adaptor
 sendSync :: D.Command -> DebugAdaptor Response
 sendSync cmd = do
   DAS{..} <- getDebugSession
-  liftIO $ do
-    putMVar syncRequests cmd
-    takeMVar syncResponses >>= \case
-      Aborted s -> error s
-      x         -> return x
+  liftIO $ putMVar syncRequests cmd
+  liftIO (takeMVar syncResponses) >>= handleAbort
 
 -- | Sends a command to the debugger, then runs the given action, and only after running the action it waits for the result of the debugger
 sendInterleaved :: D.Command -> DebugAdaptor () -> DebugAdaptor Response
@@ -25,5 +23,12 @@ sendInterleaved cmd action = do
   DAS{..} <- getDebugSession
   liftIO $ putMVar syncRequests cmd
   () <- action
-  liftIO $ takeMVar syncResponses
+  liftIO (takeMVar syncResponses) >>= handleAbort
+
+handleAbort :: Response -> DebugAdaptor Response
+handleAbort (Aborted e) = do
+  sendOutputErr (T.pack e)
+  sendTerminatedEvent defaultTerminatedEvent
+  return (Aborted e) -- will TerminatedEvent terminate this session before this happens?
+handleAbort r = return r
 
