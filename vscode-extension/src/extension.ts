3bfffb38bb60e6223505be96c009dc091141ced1
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-05-02 12:22:27 +0100

    Simplify extension code
    Code from activateMockDebug was moved and renamed to the single extension.ts file

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -12,7 +12,7 @@
 
 'use strict';
 
-import { activateMockDebug } from './activateMockDebug';
+import { WorkspaceFolder, DebugConfiguration, ProviderResult, CancellationToken } from 'vscode';
 import * as vscode from 'vscode';
 import * as cp from 'child_process';
 import * as net from 'net';
@@ -38,7 +38,113 @@ let logger : vscode.OutputChannel = vscode.window.createOutputChannel("haskell-d
 export function activate(context: vscode.ExtensionContext) {
 
     // run the debug adapter as a server inside the extension and communicate via a socket
-    activateMockDebug(context, new MockDebugAdapterServerDescriptorFactory(context));
+    let factory = new GHCDebugAdapterServerDescriptorFactory(context);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('extension.haskell-debugger-extension.runEditorContents', (resource: vscode.Uri) => {
+			let targetResource = resource;
+			if (!targetResource && vscode.window.activeTextEditor) {
+				targetResource = vscode.window.activeTextEditor.document.uri;
+			}
+			if (targetResource) {
+				vscode.debug.startDebugging(undefined, {
+					type: 'ghc-debugger',
+					name: 'Run File',
+					request: 'launch',
+					entryFile: targetResource.fsPath,
+                    entryPoint: 'main',
+                    entryArgs: [],
+                    extraGhcArgs: []
+				},
+					{ noDebug: true }
+				);
+			}
+		}),
+		vscode.commands.registerCommand('extension.haskell-debugger-extension.debugEditorContents', (resource: vscode.Uri) => {
+			let targetResource = resource;
+			if (!targetResource && vscode.window.activeTextEditor) {
+				targetResource = vscode.window.activeTextEditor.document.uri;
+			}
+			if (targetResource) {
+				vscode.debug.startDebugging(undefined, {
+					type: 'ghc-debugger',
+					name: 'Debug File',
+					request: 'launch',
+					entryFile: targetResource.fsPath,
+                    entryPoint: 'main',
+                    entryArgs: [],
+                    extraGhcArgs: []
+				});
+			}
+		}),
+		vscode.commands.registerCommand('extension.haskell-debugger-extension.toggleFormatting', (variable) => {
+			const ds = vscode.debug.activeDebugSession;
+			if (ds) {
+				ds.customRequest('toggleFormatting');
+			}
+		})
+	);
+
+	// register a configuration provider for 'ghc-debugger' debug type
+	const provider = new GHCConfigurationProvider();
+	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('ghc-debugger', provider));
+
+	context.subscriptions.push(vscode.debug.registerDebugAdapterDescriptorFactory('ghc-debugger', factory));
+	// if ('dispose' in factory) {
+	// 	context.subscriptions.push(factory);
+	// }
+
+	// override VS Code's default implementation of the debug hover
+	// here we match only Mock "variables", that are words starting with an '$'
+	// context.subscriptions.push(vscode.languages.registerEvaluatableExpressionProvider('markdown', {
+	// 	provideEvaluatableExpression(document: vscode.TextDocument, position: vscode.Position): vscode.ProviderResult<vscode.EvaluatableExpression> {
+
+	// 		const VARIABLE_REGEXP = /\$[a-z][a-z0-9]*/ig;
+	// 		const line = document.lineAt(position.line).text;
+
+	// 		let m: RegExpExecArray | null;
+	// 		while (m = VARIABLE_REGEXP.exec(line)) {
+	// 			const varRange = new vscode.Range(position.line, m.index, position.line, m.index + m[0].length);
+
+	// 			if (varRange.contains(position)) {
+	// 				return new vscode.EvaluatableExpression(varRange);
+	// 			}
+	// 		}
+	// 		return undefined;
+	// 	}
+	// }));
+
+	// override VS Code's default implementation of the "inline values" feature"
+	//context.subscriptions.push(vscode.languages.registerInlineValuesProvider('markdown', {
+
+	//	provideInlineValues(document: vscode.TextDocument, viewport: vscode.Range, context: vscode.InlineValueContext) : vscode.ProviderResult<vscode.InlineValue[]> {
+
+	//		const allValues: vscode.InlineValue[] = [];
+
+	//		for (let l = viewport.start.line; l <= context.stoppedLocation.end.line; l++) {
+	//			const line = document.lineAt(l);
+	//			var regExp = /\$([a-z][a-z0-9]*)/ig;	// variables are words starting with '$'
+	//			do {
+	//				var m = regExp.exec(line.text);
+	//				if (m) {
+	//					const varName = m[1];
+	//					const varRange = new vscode.Range(l, m.index, l, m.index + varName.length);
+
+	//					// some literal text
+	//					//allValues.push(new vscode.InlineValueText(varRange, `${varName}: ${viewport.start.line}`));
+
+	//					// value found via variable lookup
+	//					allValues.push(new vscode.InlineValueVariableLookup(varRange, varName, false));
+
+	//					// value determined via expression evaluation
+	//					//allValues.push(new vscode.InlineValueEvaluatableExpression(varRange, varName));
+	//				}
+	//			} while (m);
+	//		}
+
+	//		return allValues;
+	//	}
+	//}));
 
 }
 
@@ -46,7 +152,7 @@ export function deactivate() {
 	// nothing to do
 }
 
-class MockDebugAdapterServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
+class GHCDebugAdapterServerDescriptorFactory implements vscode.DebugAdapterDescriptorFactory {
 
     private processes = new Map<string, cp.ChildProcess>();
 	private logger: vscode.OutputChannel;
@@ -142,4 +248,36 @@ class MockDebugAdapterServerDescriptorFactory implements vscode.DebugAdapterDesc
 		this.logger.dispose();
 	}
 
+}
+
+class GHCConfigurationProvider implements vscode.DebugConfigurationProvider {
+
+	/**
+	 * Massage a debug configuration just before a debug session is being launched,
+	 * e.g. add all missing attributes to the debug configuration.
+	 */
+	resolveDebugConfiguration(folder: WorkspaceFolder | undefined, config: DebugConfiguration, token?: CancellationToken): ProviderResult<DebugConfiguration> {
+
+		// if launch.json is missing or empty
+		if (!config.type && !config.request && !config.name) {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.document.languageId === 'markdown') {
+				config.type = 'ghc-debugger';
+				config.name = 'Launch';
+				config.request = 'launch';
+				config.entryFile = '${file}';
+				config.entryPoint = 'main';
+				config.entryArgs = [];
+				config.extraGhcArgs = [];
+			}
+		}
+
+		if (!config.entryFile) {
+			return vscode.window.showInformationMessage("Cannot find an entry file to debug").then(_ => {
+				return undefined;	// abort launch
+			});
+		}
+
+		return config;
+	}
 }
