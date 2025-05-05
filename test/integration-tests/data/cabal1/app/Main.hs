590ac8f6c02885f8b334cf4bd7683e3c206abeb7
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-05-05 17:07:23 +0100

    Improve testsuite

--- test/integration-tests/data/cabal1/app/Main.hs
+++ test/integration-tests/data/cabal1/app/Main.hs
@@ -1,11 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
-
+import Control.Exception
 import System.Environment
-
 main :: IO ()
 main = do
   args <- getArgs
   putStrLn "hello"
   print args
+  fail "CATCH ME" `catch` (\(_::SomeException) -> putStrLn "caught it")
   putStrLn "goodbye"
   return ()
--- test/integration-tests/test/adapter.test.js
+++ test/integration-tests/test/adapter.test.js
@@ -1,7 +1,6 @@
 import { DebugClient } from '@vscode/debugadapter-testsupport';
 import * as cp from 'child_process';
 import * as net from 'net';
-// import assert = require('assert')
 
 function getFreePort() {
 	return new Promise((resolve, reject) => {
@@ -23,7 +22,7 @@ var dc;
 let debuggerProcess;
 
 describe("Debug Adapter Tests", function () {
-    this.timeout(20000); // 30s
+    this.timeout(5000); // 5s
     const cwd = process.cwd();
 
     beforeEach( () => getFreePort().then(port => {
@@ -36,7 +35,7 @@ describe("Debug Adapter Tests", function () {
             }, 15000); // 15 second timeout
 
             debuggerProcess.stdout.on('data', data => {
-                // console.log(data.toString())
+                console.log(data.toString())
                 const text = data.toString();
                 if (text.includes('Running')) {
                     clearTimeout(timeout);
@@ -67,20 +66,21 @@ describe("Debug Adapter Tests", function () {
       dc.stop()
     });
 
-
-    // Broken!!!
-    // let defaultMainLaunch = {
-    //         type: "ghc-debugger",
-    //         request: "launch",
-    //         name: "Haskell Debugger",
-    //         projectRoot: "data",
-    //         entryFile: "Main.hs",
-    //         entryPoint: "main",
-    //         entryArgs: ["some", "args"],
-    //         extraGhcArgs: []
-    //     }
-
-    let defaultCabalLaunch = {
+    const simpleLaunchConfigs = [
+        { name: "Vanilla config (single file)",
+          config: {
+            type: "ghc-debugger",
+            request: "launch",
+            name: "Haskell Debugger",
+            projectRoot: "data",
+            entryFile: "Main.hs",
+            entryPoint: "main",
+            entryArgs: ["some", "args"],
+            extraGhcArgs: []
+          }
+        },
+        { name: "Cabal config",
+          config : {
             type: "ghc-debugger",
             request: "launch",
             name: "Haskell Debugger",
@@ -89,165 +89,65 @@ describe("Debug Adapter Tests", function () {
             entryPoint: "main",
             entryArgs: ["some", "args"],
             extraGhcArgs: []
+          }
         }
+        // todo: Stack config?
+        // todo: hie.yaml config?
+    ]
 
-    it('should run program to the end', () => {
-        return Promise.all([
-            dc.configurationSequence(),
-            dc.launch(defaultCabalLaunch),
-            dc.waitForEvent('exited').then(e => new Promise((resolve, reject) => {
-                if (e.body.exitCode == 0)
-                    resolve(e)
-                else
-                    reject(new Error("Expecting ExitCode 0"))
-            }))
-        ]);
-    });
+    const basicTests = (launchCfg) => {
 
-    it('should stop on a breakpoint', () => {
-        return dc.hitBreakpoint(defaultCabalLaunch, { path: "app/Main.hs", line: 6 }, { path: defaultCabalLaunch.projectRoot + "/app/Main.hs", line: 6 }, { path: defaultCabalLaunch.projectRoot + "/app/Main.hs", line: 6 });
-    });
+        // The most basic functionality we test on various different
+        // configurations (such as Cabal vs without project vs Stack)
+        // The remaining tests are run only on one config since the set up
+        // starts being more specific (e.g. multiple home units with Cabal)
+        describe("Most basic functionality", function () {
 
+            describe(launchCfg.name, function () {
+
+                it('should run program to the end', () => {
+                    return Promise.all([
+                        dc.configurationSequence(),
+                        dc.launch(launchCfg.config),
+                        dc.waitForEvent('exited').then(e => new Promise((resolve, reject) => {
+                            if (e.body.exitCode == 0)
+                                resolve(e)
+                            else
+                                reject(new Error("Expecting ExitCode 1"))
+                        }))
+                    ]);
+                });
+
+                it('should stop on a breakpoint', () => {
+                    const expected = { path: launchCfg.config.projectRoot + "/" + launchCfg.config.entryFile, line: 6 }
+                    return dc.hitBreakpoint(launchCfg.config, { path: launchCfg.config.entryFile, line: 6 }, expected, expected);
+                });
+
+                it('should stop on an exception', () => {
+
+                    // const expected = { path: launchCfg.config.projectRoot + "/" + launchCfg.config.entryFile, line: 10 }
+                    // Currently, no information is provided when stopped at an exception:
+                    const expected = { }
+                    return Promise.all([
+
+                        dc.waitForEvent('initialized').then(event => {
+                            return dc.setExceptionBreakpointsRequest({
+                                filters: [ 'break-on-exception' ]
+                            });
+                        }).then(response => {
+                            return dc.configurationDoneRequest();
+                        }),
+
+                        dc.launch(launchCfg.config),
+
+                        dc.assertStoppedLocation('exception', expected)
+                    ]);
+                });
+
+            })
+        })
+    }
+
+    simpleLaunchConfigs.forEach(basicTests);
 })
 
-// test('should stop on entry', () => {
-//     return Promise.all([
-//         dc.configurationSequence(),
-//         dc.launch(defaultMainLaunch + {stopOnEntry: true}...),
-//         dc.assertStoppedLocation('entry', 1)
-//     ]);
-// });
-
-// import assert = require('assert');
-// import * as Path from 'path';
-
-// suite('Node Debug Adapter', () => {
-
-// 	const DEBUG_ADAPTER = './out/debugAdapter.js';
-
-// 	const PROJECT_ROOT = Path.join(__dirname, '../../');
-// 	const DATA_ROOT = Path.join(PROJECT_ROOT, 'src/tests/data/');
-
-
-// 	let dc: DebugClient;
-
-// 	setup( () => {
-// 		dc = new DebugClient('node', DEBUG_ADAPTER, 'mock');
-// 		return dc.start();
-// 	});
-
-// 	teardown( () => dc.stop() );
-
-
-// 	suite('basic', () => {
-
-// 		test('unknown request should produce error', done => {
-// 			dc.send('illegal_request').then(() => {
-// 				done(new Error("does not report error on unknown request"));
-// 			}).catch(() => {
-// 				done();
-// 			});
-// 		});
-// 	});
-
-// 	suite('initialize', () => {
-
-// 		test('should return supported features', () => {
-// 			return dc.initializeRequest().then(response => {
-// 				response.body = response.body || {};
-// 				assert.equal(response.body.supportsConfigurationDoneRequest, true);
-// 			});
-// 		});
-
-// 		test('should produce error for invalid \'pathFormat\'', done => {
-// 			dc.initializeRequest({
-// 				adapterID: 'mock',
-// 				linesStartAt1: true,
-// 				columnsStartAt1: true,
-// 				pathFormat: 'url'
-// 			}).then(response => {
-// 				done(new Error("does not report error on invalid 'pathFormat' attribute"));
-// 			}).catch(err => {
-// 				// error expected
-// 				done();
-// 			});
-// 		});
-// 	});
-
-// 	suite('launch', () => {
-
-// 		test('should run program to the end', () => {
-
-// 			const PROGRAM = Path.join(DATA_ROOT, 'test.md');
-
-// 			return Promise.all([
-// 				dc.configurationSequence(),
-// 				dc.launch({ program: PROGRAM }),
-// 				dc.waitForEvent('terminated')
-// 			]);
-// 		});
-
-// 		test('should stop on entry', () => {
-
-// 			const PROGRAM = Path.join(DATA_ROOT, 'test.md');
-// 			const ENTRY_LINE = 1;
-
-// 			return Promise.all([
-// 				dc.configurationSequence(),
-// 				dc.launch({ program: PROGRAM, stopOnEntry: true }),
-// 				dc.assertStoppedLocation('entry', { line: ENTRY_LINE } )
-// 			]);
-// 		});
-// 	});
-
-// 	suite('setBreakpoints', () => {
-
-// 		test('should stop on a breakpoint', () => {
-
-// 			const PROGRAM = Path.join(DATA_ROOT, 'test.md');
-// 			const BREAKPOINT_LINE = 2;
-
-// 			return dc.hitBreakpoint({ program: PROGRAM }, { path: PROGRAM, line: BREAKPOINT_LINE } );
-// 		});
-
-// 		test('hitting a lazy breakpoint should send a breakpoint event', () => {
-
-// 			const PROGRAM = Path.join(DATA_ROOT, 'testLazyBreakpoint.md');
-// 			const BREAKPOINT_LINE = 3;
-
-// 			return Promise.all([
-
-// 				dc.hitBreakpoint({ program: PROGRAM }, { path: PROGRAM, line: BREAKPOINT_LINE, verified: false } ),
-
-// 				dc.waitForEvent('breakpoint').then(event => {
-// 					const bpevent = event as DebugProtocol.BreakpointEvent;
-// 					assert.strictEqual(bpevent.body.breakpoint.verified, true, "event mismatch: verified");
-// 				})
-// 			]);
-// 		});
-// 	});
-
-// 	suite('setExceptionBreakpoints', () => {
-
-// 		test('should stop on an exception', () => {
-
-// 			const PROGRAM_WITH_EXCEPTION = Path.join(DATA_ROOT, 'testWithException.md');
-// 			const EXCEPTION_LINE = 4;
-
-// 			return Promise.all([
-
-// 				dc.waitForEvent('initialized').then(event => {
-// 					return dc.setExceptionBreakpointsRequest({
-// 						filters: [ 'otherExceptions' ]
-// 					});
-// 				}).then(response => {
-// 					return dc.configurationDoneRequest();
-// 				}),
-
-// 				dc.launch({ program: PROGRAM_WITH_EXCEPTION }),
-
-// 				dc.assertStoppedLocation('exception', { line: EXCEPTION_LINE } )
-// 			]);
-// 		});
-// 	});
-// });
