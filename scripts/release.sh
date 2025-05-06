aed705d5264dc848bb921952c26ad8dcde08592b
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-05-06 12:00:38 +0100

    Add a script for the release process
    - Prepare the release commit and friends using ./scripts/release.sh
    - Update the versions before and after using ./scripts/update-version.sh

---
+++ scripts/release.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# The most part of a release is automatically done by GitHub actions.
+# That includes (only if CI is successful):
+#   * Releasing the package on Hackage
+#   * Creating the release on GitHub
+#       * Copying the .vsix extension to the release artifacts
+#
+# The release action is triggered by a new tag matching "v${version}".
+# The "manual" part of the release is done by this script.
+# That includes:
+#   * Validating the extension version matches the Haskell package version
+#   * Creating the commit with the release keyword for that version
+#   * Tagging the release
+#
+# Note: the version must be manually updated by using ./scripts/update-version.sh
+#   * before running the release script, to the desired release version
+#   * after the release commit, to the intermediate unreleased version
+#
+# Pushing is manual to be deliberate.
+
+set -e
+
+if [ ! -f "ghc-debugger.cabal" ]; then
+  echo "Error: ghc-debugger.cabal not found. Please run this script from the project root."
+  exit 1
+fi
+
+PKG_VERSION=$(sed -nE "s/^version:\s*([0-9\.]*).*$/\1/p" ghc-debugger.cabal)
+EXTENSION_NIX_VERSION=$(sed -nE "s/^\s*version = \"(.*)\"\;$/\1/p" vscode-extension/default.nix)
+EXTENSION_PKG_VERSION=$(sed -nE "s/^\s*\"version\":\s*\"(.*)\"\,$/\1/p" vscode-extension/package.json)
+
+# Check versions match
+if [ "$PKG_VERSION" != "$EXTENSION_NIX_VERSION" ] || [ "$PKG_VERSION" != "$EXTENSION_PKG_VERSION" ]; then
+  echo "Error: Version mismatch"
+  echo "PKG_VERSION           = $PKG_VERSION"
+  echo "EXTENSION_NIX_VERSION = $EXTENSION_NIX_VERSION"
+  echo "EXTENSION_PKG_VERSION = $EXTENSION_PKG_VERSION"
+  exit 1
+fi
+
+# Nothing should be staged.
+# Git diff --quiet returns 0 if diff is empty
+if ! git diff --staged --quiet then
+  echo "Error: You have staged changes. Please commit or unstage them before proceeding."
+  exit 1
+fi
+
+# Do the release commit and tag it
+git commit --allow-empty -m "Release: $PKG_VERSION"
+git tag "v$PKG_VERSION"
+
---
+++ scripts/update-version.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+printf "Enter next version: "
+read NEW_VERSION
+
+if [ -z "$NEW_VERSION" ]; then
+  echo "Error: Version cannot be empty"
+  exit 1
+fi
+
+# Update versions
+sed -i.bkp -E "s/(^version:\s*)[0-9\.]*(\s*$)/\1$NEW_VERSION\2/" ghc-debugger.cabal
+sed -i.bkp -E "s/(^\s*version = \").*(\"\;$)/\1$NEW_VERSION\2/" vscode-extension/default.nix
+sed -i.bkp -E "s/(^\s*\"version\":\s*\").*(\"\,$)/\1$NEW_VERSION\2/" vscode-extension/package.json
+
