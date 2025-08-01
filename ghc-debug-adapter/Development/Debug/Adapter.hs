68b9d2968426ee6c0549e0269c80046db7776097
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-08-01 15:46:25 +0100

    Update to support !14480

--- ghc-debug-adapter/Development/Debug/Adapter.hs
+++ ghc-debug-adapter/Development/Debug/Adapter.hs
@@ -24,7 +24,11 @@ data DebugAdaptorState = DAS
       { syncRequests :: MVar D.Command
       , syncResponses :: MVar D.Response
       , nextFreshBreakpointId :: !BreakpointId
+#if MIN_VERSION_ghc(9,13,20250730)
+      , breakpointMap :: Map.Map GHC.InternalBreakpointId BreakpointSet
+#else
       , breakpointMap :: Map.Map GHC.BreakpointId BreakpointSet
+#endif
       , entryPoint :: String
       , entryArgs :: [String]
       , projectRoot :: FilePath
