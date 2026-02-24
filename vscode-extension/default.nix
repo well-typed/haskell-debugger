9bc353e58a753df549167557119cfe38a6f1b514
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-02-24 11:55:38 +0000

    Bump version to 0.12.1.0

--- vscode-extension/default.nix
+++ vscode-extension/default.nix
@@ -2,7 +2,7 @@
 
 pkgs.buildNpmPackage {
     pname = "haskell-debugger-extension";
-    version = "0.12.0";
+    version = "0.12.1";
     src = ./.;
 
     npmDepsHash = "sha256-rvPlvEsFygi/EYh0vcOBDAC4Sf5nzJIfaN8HjdsVXE0=";
