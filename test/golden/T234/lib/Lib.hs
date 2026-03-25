7930af337ba53b57087f7b72cf8e827e01d886e7
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-03-25 15:15:44 +0000

    test: Add newtype debug view tests for #234
    This is a regression test for #234, where DebugView instances for
    newtype constructors don't work.
    
    These duplicate T130c, but use a `newtype` instead of `data`.

---
+++ test/golden/T234/lib/Lib.hs
@@ -0,0 +1,20 @@
+module Lib (mainish) where
+
+import Data.List (intercalate)
+import GHC.Debugger.View.Class
+
+newtype ColonList = ColonList [String]
+  deriving Show
+
+instance DebugView ColonList where
+  debugValue (ColonList xs) = simpleValue (intercalate ":" xs) False
+  debugFields _ = pure (VarFields [])
+
+mainish :: IO ()
+mainish = do
+  let value = mkValue
+  print value
+
+mkValue :: ColonList
+mkValue = ColonList ["alpha", "beta", "gamma"]
+{-# OPAQUE mkValue #-}
