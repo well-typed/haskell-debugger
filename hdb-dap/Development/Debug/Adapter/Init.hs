e5f93b2f93a4fe3882621ef61719f498aa700d3f
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-08-31 17:42:13 +0100

    Receive externalInterpreterArgs in launch.json
    Makes it easier to toggle -v3 for the run-in-terminal external
    interpreter, and also options like +RTS -DS -Di and friends

--- hdb-dap/Development/Debug/Adapter/Init.hs
+++ hdb-dap/Development/Debug/Adapter/Init.hs
@@ -78,6 +78,9 @@ data LaunchArgs
     -- ^ Additional arguments to pass to the GHC invocation inferred by hie-bios for this project
   , cradleFile :: Maybe FilePath
     -- ^ specify cradle file rather than let it be inferred from @entryFile@, relative to @projectRoot@.
+  , externalInterpreterArgs :: Maybe [String]
+    -- ^ Extra arguments to append to the external interpreter invocation. No
+    -- effect when using the internal interpreter.
   } deriving stock (Show, Eq, Generic)
     deriving anyclass FromJSON
 
@@ -108,6 +111,7 @@ initDebugger l0 servConf interpChoice
                          , entryArgs  = fromMaybe [] -> entryArgs
                          , extraGhcArgs = fromMaybe [] -> extraGhcArgs
                          , cradleFile
+                         , externalInterpreterArgs = fromMaybe [] -> externalInterpreterArgs
                          } = do
   syncRequests  <- liftIO newEmptyMVar
   syncResponses <- liftIO newEmptyMVar
@@ -155,7 +159,7 @@ initDebugger l0 servConf interpChoice
 
       dbgLog <- liftIO $ createDebuggerLogger l dapLogger writeDAPOutput
 
-      dapd <- initDAPDebuggee l (hdbProgram servConf) interpChoice
+      dapd <- initDAPDebuggee l (hdbProgram servConf) interpChoice externalInterpreterArgs
 
       let
         defaultRunConf = Debugger.RunDebuggerSettings
@@ -205,14 +209,15 @@ initDAPDebuggee
   :: LogAction IO DAPSessionLog
   -> FilePath
   -> InterpreterChoice
+  -> [String]
   -> DebugAdaptor DAPDebuggee
-initDAPDebuggee _ _ InterpreterChoice{runInTerminal = False, internal = True}
+initDAPDebuggee _ _ InterpreterChoice{runInTerminal = False, internal = True} _
   = internalNoInTerminalDAPD
-initDAPDebuggee _ hdbProg InterpreterChoice{runInTerminal = False, internal = False}
-  = externalNoInTerminalDAPD hdbProg
-initDAPDebuggee _ hdbProg InterpreterChoice{internal = False, runInTerminal = True}
-  = externalInTerminalDAPD hdbProg
-initDAPDebuggee l hdbProg InterpreterChoice{runInTerminal = True, internal = True}
+initDAPDebuggee _ hdbProg InterpreterChoice{runInTerminal = False, internal = False} extraInterpArgs
+  = externalNoInTerminalDAPD hdbProg extraInterpArgs
+initDAPDebuggee _ hdbProg InterpreterChoice{internal = False, runInTerminal = True} extraInterpArgs
+  = externalInTerminalDAPD hdbProg extraInterpArgs
+initDAPDebuggee l hdbProg InterpreterChoice{runInTerminal = True, internal = True} _
   = internalInTerminalDAPD l hdbProg
 
 
