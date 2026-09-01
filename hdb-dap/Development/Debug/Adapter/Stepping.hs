230fb960372600294aa22288639bcace2576118f
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2026-09-01 11:16:44 +0100

    Multi-threaded debugging first steps
    This patch moves us closer to multi-threaded debugging.
    
    The key changes:
    
    1. Make the resume command (continue/step-in,next,out) consider all
    aspects of per-thread vs global resuming and always receiving a
    thread-id of the "active thread". See `DoResume`, `ResumeStep`,
    `ResumeTheWorld`. This patch doesn't yet respect `ResumeTheWorld`,
    just makes `ResumeStep` be applied to the given thread.
    
    ```haskell
        -- Examples:
        --
        -- - To "global continue": @DoResume <active tid> ResumeNoStep ResumeTheWorld@
        -- (in this case, the active tid is of no consequence, but still passed)
        --
        -- - To "single-thread step in": @DoResume <a tid> ResumeSingleStep ResumeJustThisThread@
        --
        -- - To "global step-next": @DoResume <a tid> ResumeStepLocal ResumeTheWorld@;
        --   All threads will be resumed and the specified thread will break at the
        --   next local breakpoint.
        | DoResume RemoteThreadId ResumeStep ResumeTheWorld
    ```
    
    2. In GHC 10.1, use `execIsolateMode = MultiThreadedBreaks`. This part
       isn't yet upstreamed in GHC.
    
       In the patch which introduces this option, GHC becomes capable of
       having many threads hit breakpoints individually and not mix
       everything up because of races to the breakMVar which used to be
       global instead of per-thread. This is crucial for correctness.
    
       This option makes the execution stop if any thread hits a breakpoint,
       and reports the first thread which hits the breakpoint. Subsequent
       steps will display non-deterministically other threads which may be
       stopped on breakpoints, depending on which wins the race to write the
       debugger Status MVar.
    
    3. In that GHC patch as well, each breakpoint hit returns the 'Resume'
       context for that breakpoint, which allows us to keep a per-thread
       resume context mapping in the debugger.
    
       In 9.14.{1,2}, which we still support, we pop the first resume off of
       `ic_resume` and consider it the resume of the thread which has just
       hit a brekapoint. This is true as long as we don't have more than one
       thread running at the same time. Otherwise, the `Resume` context may
       not match the thread we just resumed.
       In essence, for 9.14.2, we keep the same existing behavior -- which
       is not safe in a multi-threaded scenario.
    
       Resume commands now receive an explict `Resume` context which in 10.1
       is guaranteed to be correct, and 9.14.2 is correct for
       single-threaded debugging. Therefore, in <= 9.14.2, we disable
       "supportsSingleThreadedCapability"
    
    Follow up to #364
    Towards #221 and #138

--- hdb-dap/Development/Debug/Adapter/Stepping.hs
+++ hdb-dap/Development/Debug/Adapter/Stepping.hs
@@ -1,4 +1,11 @@
-module Development.Debug.Adapter.Stepping where
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
+module Development.Debug.Adapter.Stepping
+  ( commandContinue
+  , commandNext
+  , commandStepIn
+  , commandStepOut
+  ) where
 
 import DAP
 
@@ -10,27 +17,39 @@ import Development.Debug.Adapter.Evaluation
 
 commandContinue :: DebugAdaptor ()
 commandContinue = do
+  ContinueArguments {..} <- getArguments
+  let rtw = bool_single_thrd continueArgumentsSingleThread
+  let rsm = DoResume (RemoteThreadId continueArgumentsThreadId) ResumeNoStep rtw
   resetObjectReferences
-  DidContinue er <- sendInterleaved DoContinue $
-    sendContinueResponse (ContinueResponse True)
+  DidResume er <- sendInterleaved rsm $
+    sendContinueResponse (ContinueResponse (maybe True not continueArgumentsSingleThread))
   handleEvalResult False er
 
 commandNext :: DebugAdaptor ()
 commandNext = do
+  NextArguments {..} <- getArguments
+  let rtw = bool_single_thrd nextArgumentsSingleThread
+  let rsm = DoResume (RemoteThreadId nextArgumentsThreadId) ResumeStepLocal rtw
   resetObjectReferences
-  DidStep er <- sendInterleaved DoStepLocal sendNextResponse
+  DidResume er <- sendInterleaved rsm sendNextResponse
   handleEvalResult True er
 
 commandStepIn :: DebugAdaptor ()
 commandStepIn = do
+  StepInArguments {..} <- getArguments
+  let rtw = bool_single_thrd stepInArgumentsSingleThread
+  let rsm = DoResume (RemoteThreadId stepInArgumentsThreadId) ResumeSingleStep rtw
   resetObjectReferences
-  DidStep er <- sendInterleaved DoSingleStep sendStepInResponse
+  DidResume er <- sendInterleaved rsm sendStepInResponse
   handleEvalResult True er
 
 commandStepOut :: DebugAdaptor ()
 commandStepOut = do
+  StepOutArguments {..} <- getArguments
+  let rtw = bool_single_thrd stepOutArgumentsSingleThread
+  let rsm = DoResume (RemoteThreadId stepOutArgumentsThreadId) ResumeStepOut rtw
   resetObjectReferences
-  DidStep er <- sendInterleaved DoStepOut sendStepOutResponse
+  DidResume er <- sendInterleaved rsm sendStepOutResponse
   handleEvalResult True er
 
 --------------------------------------------------------------------------------
@@ -43,3 +62,12 @@ resetObjectReferences = do
       , breakpointMap = mempty
       , variablesMap  = mempty
       }
+
+bool_single_thrd :: Maybe Bool -> ResumeTheWorld
+bool_single_thrd = bool_mby ResumeJustThisThread ResumeTheWorld
+
+bool_mby :: a {- if Just True -} -> a {- if Nothing or Just False -} -> Maybe Bool -> a
+bool_mby l r = \case
+  Nothing    -> r
+  Just False -> r
+  Just True  -> l
